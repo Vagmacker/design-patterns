87ff496983b252c5a8687a0c59f939d59d54b59f
vagmacker <vagmackerdsm@gmail.com>
2024-04-21 11:32:32 -0300

    feat: :sparkles: add more test about state pattern

--- src/gof/behavioral/state/TicketStatus.test.ts
+++ src/gof/behavioral/state/TicketStatus.test.ts
@@ -1,20 +1,11 @@
+import {
+  AssignedStatus,
+  RequestedStatus,
+  InProgressStatus,
+} from "./TicketStatus";
 import Ticket from "./Ticket";
-import { RequestedStatus } from "./TicketStatus";
 
 describe("TicketStatus Unit Test", (): void => {
-  it("should transit to assigned status when calls assign", () => {
-    // given
-    const expectedStatus = "assigned";
-    const expectedTicket = new Ticket();
-    const target = new RequestedStatus(expectedTicket);
-
-    // when
-    target.assign();
-
-    // then
-    expect(expectedStatus).toBe(expectedTicket.getStatus().value);
-  });
-
   it("should transit to assigned status when calls assign", () => {
     // given
     const expectedStatus = "assigned";
@@ -26,6 +17,38 @@ describe("TicketStatus Unit Test", (): void => {
 
     // when
     target.assign();
+
+    // then
+    expect(expectedStatus).toBe(expectedTicket.getStatus().value);
+  });
+
+  it("should transit to in progress status when calls start", () => {
+    // given
+    const expectedStatus = "in_progress";
+    const expectedTicket = new Ticket();
+
+    expect(expectedTicket.status.value).toBe("request");
+
+    const target = new AssignedStatus(expectedTicket);
+
+    // when
+    target.start();
+
+    // then
+    expect(expectedStatus).toBe(expectedTicket.getStatus().value);
+  });
+
+  it("should transit to closed status when calls start", () => {
+    // given
+    const expectedStatus = "closed";
+    const expectedTicket = new Ticket();
+
+    expect(expectedTicket.status.value).toBe("request");
+
+    const target = new InProgressStatus(expectedTicket);
+
+    // when
+    target.close();
 
     // then
     expect(expectedStatus).toBe(expectedTicket.getStatus().value);
